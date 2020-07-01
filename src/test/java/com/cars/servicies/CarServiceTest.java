352ca80760ce59d714d4c5c5b518d8e14c723643
Wellisson Gomes <wellisson.cacho@ccc.ufcg.edu.br>
2020-07-01 18:02:52 -0300

    add test

---
+++ src/test/java/com/cars/servicies/CarServiceTest.java
@@ -0,0 +1,49 @@
+package com.cars.servicies;
+
+import com.cars.dto.CarDTO;
+import com.cars.entities.Car;
+import com.cars.exceptions.ObjectNotFoundException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CarServiceTest {
+    @Autowired
+    private CarService carService;
+
+    @Test
+    void insertCar() {
+        Car car = new Car();
+        car.setName("FUSQUINHA DA MASSA");
+        car.setType("esportivo");
+
+        CarDTO carDTO = carService.save(car);
+        assertNotNull(carDTO);
+
+        Long id = carDTO.getId();
+        assertNotNull(id);
+
+        CarDTO c = carService.getById(id);
+        assertNotNull(c);
+
+        assertEquals(c.getName(), "FUSQUINHA DA MASSA");
+        assertEquals(c.getType(), "esportivo");
+
+        // pra não ficar no db (TODO: fazer um bd fake pra evitar isso)
+        carService.deleteById(id);
+        try {
+            carService.getById(id);
+            fail("não deveria existir mais o carro");
+        }catch (ObjectNotFoundException onfe) {
+            // ok
+        }
+    }
+
+    @Test
+    void getCar() {
+
+    }
+}
+
