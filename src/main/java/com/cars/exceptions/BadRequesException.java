232d2e178f803e65c88ba803558ae7b3355a4554
Wellisson Gomes <wellisson.cacho@ccc.ufcg.edu.br>
2020-06-30 22:24:02 -0300

    add BadRequestException

---
+++ src/main/java/com/cars/exceptions/BadRequesException.java
@@ -0,0 +1,10 @@
+package com.cars.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+public class BadRequesException extends RuntimeException {
+    public BadRequesException() {
+    }
+}
