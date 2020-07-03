9ea4f90b657447341c740101818095bde6c5e0f5
Wellisson Gomes <wellisson.cacho@ccc.ufcg.edu.br>
2020-07-03 15:51:41 -0300

    add a new test

--- src/test/java/com/cars/servicies/CarServiceTest.java
+++ src/test/java/com/cars/servicies/CarServiceTest.java
@@ -2,10 +2,12 @@ package com.cars.servicies;
 
 import com.cars.dto.CarDTO;
 import com.cars.entities.Car;
-import com.cars.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -30,6 +32,21 @@ class CarServiceTest {
 
         assertEquals(c.getName(), "FUSQUINHA DA MASSA");
         assertEquals(c.getType(), "esportivo");
+    }
+
+    @Test
+    void getCars() {
+        for(int i = 0; i < 9; i++) {
+            Car car = new Car();
+            car.setName("FUSQUINHA DA MASSA");
+            car.setType("esportivo " + (i + 1));
+            CarDTO carDTO = carService.save(car);
+        }
+
+        List<CarDTO> carDTOList = carService.getAll();
+        assertFalse(carDTOList.isEmpty());
+
+        assertEquals(10, carDTOList.size());
     }
 }
 
